36b52248327ad0340638c260f72ac6ccf79ab6a9
Omar Navarro <jesusbalderas@cloud.upwork.com>
2018-11-04 21:32:35 -0600

    Refactors code using a few functions

--- quiz/main.js
+++ quiz/main.js
@@ -11,12 +11,25 @@ const heroes = new Map(characters);
 let score = 0;
 
 for(const [alterEgo, person] of heroes.entries()) {
-  const response = prompt(`What is ${alterEgo}'s real name?`);
-  if(response === person) {
+  const response = ask(alterEgo);
+  check(response, person);
+}
+gameOver();
+
+
+function ask(alterEgo) {
+  return prompt(`What is ${alterEgo}'s real name?`);
+}
+
+function check(response, answer) {
+  if(response === answer) {
     alert('Correct!');
     score++;
   } else {
-    alert (`Wrong! The correct answer was ${person}.`);
+    alert (`Wrong! The correct answer was ${answer}.`);
   }
+}  
+
+function gameOver() {
+  alert(`Your score is ${score}.`);
 }
-alert(`Your score is ${score}.`);
